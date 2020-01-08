68ccdb24e5fc01ec94af85e5561fdef332993386
ericlugo <ericlugo@users.noreply.github.com>
2020-01-08 01:55:41 -0500

    update dependencies and modify `gatsby-config.js`
    config:
    - add gatsby-plugin-sass for ease of styling
    - add gatsby-plugin-sharp & gatsby-transformer-sharp for gatsby-image
      & gatsby-background-image for out-of-the-box image optimization
    - add gatsby-plugin-transition-link for easy and powerful site
      transitions
    - add gatsby-plugin-react-helmet for metadata management and site seo
    - add gatsby-plugin-netlify-cms for later ease of use
    - add gatsby-robots-txt to autogenerate robots.txt file
    - add gatsby-plugin-sitemap to autogenerate sitemap.xml
    
    dependencies installed:
    - gatsby-background-image
    - gatsby-image
    - gatsby-plugin-netlify-cms (needs netlify-cms-app)
    - gatsby-plugin-react-helmet (needs react-helmet)
    - gatsby-plugin-robots-txt
    - gatsby-plugin-sass (needs node-sass)
    - gatsby-plugin-sharp (for gatsby-image & gatsby-background-image)
    - gatsby-plugin-sitemap
    - gatsby-plugin-transition-link (needs GSAP for AniLink)
    - gatsby-source-filesystem
    - gatsby-transformer-sharp (for gatsby-image & gatsby-background-image)
    - gsap
    - netlify-cms-app
    - node-sass
    - react-helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,44 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Eric SarragaLugo`,
+    description: `Hi, I'm Eric! Are you looking for a team member you can count on? I love to delve into complex topics and thrive when working to overcome challenges. Check out my portfolio and send me a message!`,
+    author: `Eric SarragaLugo`,
+    // avatar: ``,
+    siteUrl: `https://ericlugo.me/`,
+    // twitter: ``,
+    // codepen: ``,
+    // github: ``,
+  },
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-react-helmet`,
+    "gatsby-plugin-robots-txt",
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-transition-link`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: "https://ericlugo.me/",
+        sitemap: "https://ericlugo.me/sitemap.xml",
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
 }
